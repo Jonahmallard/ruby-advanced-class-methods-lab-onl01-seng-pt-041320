49c8f442254afce0b5ede0fb51c12a9ecd357e97
Jonah Mallard <benjaminjmallard@gmail.com>
2020-05-10 01:20:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,12 @@ class Song
   end
 
 end
+
+  def new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song.save
+    song
+  end
+
